f0835d3a70749d28cc3eb35fbfb68ca8627f0bd5
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-03-13 13:56:23 +0100

    Changed message 1

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! This is CD live baby!'
+    return 'Hello World! This is CD live baby! Yet another test'
 
 if __name__ == "__main__":
     app.run()
